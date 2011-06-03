7f15c0c41c48236bc50a3e2cdebd2875cedd9233
totycro <totycro@unknown-horizons.org>
2011-06-03 18:10:46 +0200

    x

--- format_testdata.py
+++ format_testdata.py
@@ -13,10 +13,10 @@ l.sort(key=operator.itemgetter(0))
 
 def format_lage(s):
 	d = {
-			'S': "am Strand",
-			'L': "am Land",
-			'SR': "am Siedlungsrand",
-			'SN': u"in Strandn\"ahe"
+			'S': "Strand",
+			'L': "Land",
+			'SR': "Siedlungsrand",
+			'SN': u"Strandn\"ahe"
 			}
 	return d[s]
 
@@ -27,28 +27,53 @@ def format_geb(s):
 			}
 	return d[s]
 			
-def format_num(n):
-	return n[:7]
+def format_num(n, x=5):
+	n = float(n.replace(",", "."))
+	return '%F' % n # round(n, x) 
 
-def format_percent(p):
+def format_percent(p, add_plus=False):
 	p = p.replace(",", ".")
-	return str(int(float(p) * 100)) + "\%"
+	val = round(float(p) * 100, 1)
+	val_str = str(val)+ "\%"
+	if add_plus and val > 0:
+		val_str = "+"+val_str
+	return val_str
+
+def format_counter(c):
+	s = "UC"
+	if c < 10: s +="0"
+	s += str(c)
+	return s
 
 counter = 0
+tab1 = ""
+tab2 = ""
 for i in l:
 	counter += 1
 	#if counter == 10: import pudb ; pudb.set_trace()
-	out = ""
-	out += str(counter) + " & "
-	out += i[0] + " " + format_lage(i[1]) + ', ' + format_geb(i[2]) + ' & '
+	out1 = ""
+	out2 = ""
 
-	for j in xrange(5, 10):
-		out += format_num(i[j]) + ' & ' 
+	out1 += format_counter(counter) + " & "
+	out2 += format_counter(counter) + " & "
 
-	out += format_percent(i[10])
+	out1 += i[0] + " & " + format_lage(i[1]) + ' & ' + format_geb(i[2]) 
 
-	out += "\\\\"
-	print out
+	for j in xrange(5, 9):
+		out2 += format_num(i[j]) + ' & ' 
+
+	out2 += format_num(i[9], 6) + ' & '
+	out2 += format_percent(i[10])
+
+	out1 += "\\\\"
+	out2 += "\\\\"
+
+	tab1 += out1 + "\n"
+	tab2 += out2 + "\n"
+
+print tab1
+print
+print tab2
 
 
 
